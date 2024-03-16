87e9843f4160722a5bf20750ab2aa36424a4e262
jvitormelo <jvitor.ovidio2@hotmail.com>
2024-03-16 03:14:55 -0300

    Update layout component and navbar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 import { Navlinks } from "@/components/nav-links";
 import { SearchCommand } from "@/components/search-command";
 import { ModeToggle } from "@/components/theme-toggle";
-import { UserAvatar } from "@/components/ui/avatar";
+import { Avatar, UserAvatar } from "@/components/ui/avatar";
 import { UserInfo } from "@/components/user-info";
 import { Suspense } from "react";
 
@@ -9,7 +9,7 @@ function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <section className="mx-auto grid w-full max-w-7xl items-start gap-6 px-6 py-6 md:grid-cols-12">
+      <section className="mx-auto grid w-full max-w-7xl items-start gap-6 p-2 md:grid-cols-12  lg:p-6">
         <Sidebar />
         <main className="grid min-h-[90vh] w-full gap-6 md:col-span-9">
           <div className="space-y-6">{children}</div>
@@ -22,8 +22,8 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 function Navbar() {
   return (
     <div className="sticky top-0 z-50 border-b bg-background py-2">
-      <header className="mx-auto flex max-w-7xl justify-between px-6">
-        <div>LOGO</div>
+      <header className="mx-auto flex max-w-7xl items-center justify-between px-2 lg:px-6">
+        <Avatar />
 
         <SearchCommand />
 
